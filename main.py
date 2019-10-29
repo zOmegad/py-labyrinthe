21b4c13c230e3e36d41b4cf2e712e72b6a990420
Omegad <simonrichard1998@gmail.com>
2019-10-29 16:13:27 +0100

    v1 terminée

--- main.py
+++ main.py
@@ -13,7 +13,77 @@ def begin():
 
 def start(level):
 	print("Bienvenue dans mon labyrinthe, c'est partit : \n")
+	enum = 0
+	start_level = []
 	for line in level:
-		print(''.join(map(str, line)))  # transforme chaque list en string
+		x = list(line)
+		start_level.insert(enum, x)
+		print(''.join(map(str, line)))  # transforme chaques list en string
+		enum += 1
+	play(start_level)
+
+def play(start_level):
+	print("Utilisez Z Q S D pour déplacer votre personage et atteindre la fin du niveau, bonne chance")
+	finish = False
+	position_x = 0
+	position_y = 0
+	while finish != True:
+		direction = input()
+		direction = direction.lower()
+		if direction == "d":
+			position_y += 1
+			if start_level[position_x][position_y] == "#":
+				print(position_y)
+				print("Impossible d'aller ici")
+				position_y -=1
+			else:
+				if start_level[position_x][position_y] == '=' : finish = True
+				start_level[position_x][position_y - 1] = " "
+				start_level[position_x][position_y] = "X"
+				for line in start_level:
+					print(''.join(map(str, line)))  # transforme chaques list en string
+		elif direction == "q":
+			position_y -= 1
+			if start_level[position_x][position_y] == "#":
+				print(position_y)
+				print("Impossible d'aller ici")
+				position_y +=1
+			else:
+				if start_level[position_x][position_y] == '=' : finish = True
+				start_level[position_x][position_y + 1] = " "
+				start_level[position_x][position_y] = "X"
+				for line in start_level:
+					print(''.join(map(str, line)))  # transforme chaques list en string
+		elif direction == "z":
+			position_x -= 1
+			if start_level[position_x][position_y] == "#":
+				print(position_y)
+				print("Impossible d'aller ici")
+				position_x +=1
+			else:
+				if start_level[position_x][position_y] == '=' : finish = True
+				start_level[position_x + 1][position_y] = " "
+				start_level[position_x][position_y] = "X"
+				for line in start_level:
+					print(''.join(map(str, line)))  # transforme chaques list en string
+		elif direction == "s":
+			position_x += 1
+			if start_level[position_x][position_y] == "#":
+				print(position_y)
+				print("Impossible d'aller ici")
+				position_x -=1
+			else:
+				if start_level[position_x][position_y] == '=' : finish = True
+				start_level[position_x - 1][position_y] = " "
+				start_level[position_x][position_y] = "X"
+				for line in start_level:
+					print(''.join(map(str, line)))  # transforme chaques list en string
+		else:
+			print("Utilise Z Q S ou D")
+
+	finished()
+
+def finished():
+	print("Felicitation vous avez finit le labyrinthe")
 
 begin()

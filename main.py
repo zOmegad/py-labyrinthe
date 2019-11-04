6c998a58cdb6e6cb2003a1b53d21060b88d9f591
Omegad <simonrichard1998@gmail.com>
2019-11-04 20:02:40 +0100

    move function

--- main.py
+++ main.py
@@ -24,62 +24,58 @@ def show_level(level):
 	for line in level:
 		print(''.join(map(str, line)))  # transforme chaques list en string
 
-def play(start_level):
-	print("Utilisez Z Q S D pour déplacer votre personage et atteindre la fin du niveau, bonne chance")
-	finish = False
-	position = {"x": 0, "y": 0}
-	while finish != True:
-		print(position["x"])
-		print(position["y"])
-		direction = input()
-		direction = direction.lower()
-		if direction == "d":
-			position["y"] += 1
-			if start_level[position["x"]][position["y"]] == "#":
-				print(position["y"])
-				print("Impossible d'aller ici")
-				position["y"] -= 1
-			else:
-				if start_level[position["x"]][position["y"]] == '=' : finish = True
-				start_level[position["x"]][position["y"] - 1] = " "
-				start_level[position["x"]][position["y"]] = "X"
-				show_level(start_level)
-		elif direction == "q":
-			position["y"] -= 1
-			if start_level[position["x"]][position["y"]] == "#":
-				print("Impossible d'aller ici")
-				positionv["y"] += 1
-			else:
-				if start_level[position["x"]][position["y"]] == '=' : finish = True
-				start_level[position["x"]][position["y"] + 1] = " "
-				start_level[position["x"]][position["y"]] = "X"
-				show_level(start_level)
-		elif direction == "z":
-			position["x"] -= 1
-			if start_level[position["x"]][position["y"]] == "#":
-				print("Impossible d'aller ici")
-				position["x"] += 1
-			else:
-				if start_level[position["x"]][position["y"]] == '=' : finish = True
-				start_level[position["x"] + 1][position["y"]] = " "
-				start_level[position["x"]][position["y"]] = "X"
-				show_level(start_level)
-		elif direction == "s":
-			position["x"] += 1
-			if start_level[position["x"]][position["y"]] == "#":
-				print("Impossible d'aller ici")
-				position["x"] -= 1
-			else:
-				if start_level[position["x"]][position["y"]] == '=' : finish = True
-				start_level[position["x"] - 1][position["y"]] = " "
-				start_level[position["x"]][position["y"]] = "X"
-				show_level(start_level)
-		else:
-			print("Utilise Z Q S ou D")
+def move(start_level, position, finish, edit_pos, direction, suppression):
+    if direction == "right_down" : position[edit_pos] += 1
+    if direction == "left_up" : position[edit_pos] -= 1
+    if start_level[position["x"]][position["y"]] == "#": 
+        print(position["y"])
+        print("Impossible d'aller ici")
+        if position == "right_left" : position[edit_pos] -= 1
+    else:
+        if start_level[position["x"]][position["y"]] == "=" : finish[0] = True
+        if suppression == "right":
+            start_level[position["x"]][position["y"] - 1] = " "
+            start_level[position["x"]][position["y"]] = "X"
+            print(finish)
+        elif suppression == "left":
+            start_level[position["x"]][position["y"] + 1] = " "
+            start_level[position["x"]][position["y"]] = "X"
+        elif suppression == "up":
+            start_level[position["x"] + 1][position["y"]] = " "
+            start_level[position["x"]][position["y"]] = "X"
+        elif suppression == "down":
+            start_level[position["x"] - 1][position["y"]] = " "
+            start_level[position["x"]][position["y"]] = "X"
+        else:
+        	pass
 
-	finished()
+def play(start_level):
+    print("Utilisez Z Q S D pour déplacer votre personage et atteindre la fin du niveau, bonne chance")
+    finish = [False]
+    position = {"x": 0, "y": 0}
+    while finish[0] == False:
+        print(position["x"])
+        print(position["y"])
+        direction = input()
+        direction = direction.lower()
+        if direction == "d":
+            move(start_level, position, finish, edit_pos="y", direction="right_down", suppression="right")
+            show_level(start_level)
+        elif direction == "q":
+            move(start_level, position, finish, edit_pos="y", direction="left_up", suppression="left")
+            show_level(start_level)
+        elif direction == "z":
+            move(start_level, position, finish, edit_pos="x", direction="left_up", suppression="up")
+            show_level(start_level)
+        elif direction == "s":
+            move(start_level, position, finish, edit_pos="x", direction="right_down", suppression="down")
+            show_level(start_level)
+        else:
+            print("Utilise Z Q S ou D")
+
+    finished()
 
 def finished():
-	print("Felicitation vous avez finit le labyrinthe")
+    print("Felicitation vous avez finit le labyrinthe")
 
 begin()

7f97d7f72d8efa6f217eaf0b58ecb50c39b88aad
Omegad <simonrichard1998@gmail.com>
2019-11-08 16:19:38 +0100

    random item function edit

--- Item.py
+++ Item.py
@@ -10,4 +10,15 @@ class Item:
         for item in self.items:
             random_position_x = random.randint(1,14)
             random_position_y = random.randint(1,20)
-            my_map.start_level[random_position_x][random_position_y] = item
+            i = False
+            while i == False:
+                if my_map.start_level[random_position_x][random_position_y] == " ":
+                    my_map.start_level[random_position_x][random_position_y] = item
+                    i = True
+                elif my_map.start_level[random_position_x][random_position_y] == "#" and random_position_x < 14 and random_position_y < 20:
+                    random_position_x += 1
+                    if my_map.start_level[random_position_x][random_position_y] == "#" and random_position_x < 14 and random_position_y < 20:
+                        random_position_y += 1
+                    else:
+                        my_map.start_level[random_position_x][random_position_y] = item
+                        i = True

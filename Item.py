dceabc49d7b782c6f847c35bee0f0a9485223776
Omegad <simonrichard1998@gmail.com>
2019-11-14 17:16:31 +0100

    New place_item function

--- Item.py
+++ Item.py
@@ -1,4 +1,5 @@
 import random
+from random import randrange
 
 class Item:
     
@@ -8,17 +9,19 @@ class Item:
     def place_item(self, my_map):
 
         for item in self.object:
+            # chiffre aléatoire pour avoir une des listes
             random_position_x = random.randint(1,14)
-            random_position_y = random.randint(1,20)
-            i = False
-            while i == False:
-                if my_map.start_level[random_position_x][random_position_y] == " ":
-                    my_map.start_level[random_position_x][random_position_y] = item
-                    i = True
-                elif my_map.start_level[random_position_x][random_position_y] == "#" and random_position_x < 14 and random_position_y < 20:
-                    random_position_x += 1
-                    if my_map.start_level[random_position_x][random_position_y] == "#" and random_position_x < 14 and random_position_y < 20:
-                        random_position_y += 1
-                    else:
-                        my_map.start_level[random_position_x][random_position_y] = item
-                        i = True
+
+            # on prend une des valeurs de la liste
+            random_index = randrange(len(my_map.start_level[random_position_x]))
+            
+            # on récupère la valeure
+            random_item = my_map.start_level[random_position_x][random_index]
+
+            # tant que la valeur n'est pas " ", on en prend une nouvelle dans la même list
+            while random_item != " ":
+                random_index = randrange(len(my_map.start_level[random_position_x]))
+                random_item = my_map.start_level[random_position_x][random_index]
+
+            # et enfin on remplace la valeur par l'item    
+            my_map.start_level[random_position_x][random_index] = item

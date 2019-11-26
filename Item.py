e191f30db7806f9361c5f8d1d52bc071d554f28b
Omegad <simonrichard1998@gmail.com>
2019-11-26 15:16:17 +0100

    pep8

--- Item.py
+++ Item.py
@@ -1,27 +1,29 @@
 import random
 from random import randrange
 
+
 class Item:
-    
+
     def __init__(self):
         self.object = ["A", "B", "C"]
 
     def place_item(self, my_map):
 
         for item in self.object:
-            # chiffre aléatoire pour avoir une des listes
-            random_position_x = random.randint(1,14)
+            # random number to get a list
+            random_position_x = random.randint(1, 14)
 
-            # on prend une des valeurs de la liste
-            random_index = randrange(len(my_map.start_level[random_position_x]))
-            
-            # on récupère la valeure
+            # we take a value of the list
+            random_index = randrange(
+                len(my_map.start_level[random_position_x]))
+
+            # we take the value
             random_item = my_map.start_level[random_position_x][random_index]
 
-            # tant que la valeur n'est pas " ", on en prend une nouvelle dans la même list
+            # while the value isn't " " we take another one
             while random_item != " ":
-                random_index = randrange(len(my_map.start_level[random_position_x]))
+                random_index = randrange(
+                    len(my_map.start_level[random_position_x]))
                 random_item = my_map.start_level[random_position_x][random_index]
 
-            # et enfin on remplace la valeur par l'item    
             my_map.start_level[random_position_x][random_index] = item
